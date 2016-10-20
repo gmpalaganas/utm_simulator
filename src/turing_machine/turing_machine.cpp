977702d7c5343183d863338b2e4a3e66807f39be
Genesis Ian Palaganas <palaganas.genesis@gmail.com>
2016-10-21 02:05:04 +0800

    Add class for simulating Turing Machine

---
+++ src/turing_machine/turing_machine.cpp
@@ -0,0 +1,64 @@
+#include "turing_machine.hpp"
+
+using namespace std;
+using namespace TuringMachine;
+
+TuringMachine(vector<string> states, string input_alphabet, vector<vector<int>> state_transition_matrix,
+              string start_state, vector<string> accept_states, string tape, char blank_symbol = '_'
+              int head_pos = 0) :
+    states(states),
+    input_alphabet(input_alphabet),
+    state_transition_matrix(state_transition_matrix),
+    accept_states(accept_states),
+    tape(tape),
+    blank_symbol(blank_symbol),
+    head_pos(head_pos){
+
+        set_cur_state(start_state)
+
+}
+
+TuringMachine(ifstream stream){
+
+    string holder;
+
+    getline(stream, input_alphabet);
+
+    getline(stream, holder);
+    states = utils::split_string(holder, ' ');
+
+    getline(stream,holder);
+    set_cur_state(holder);
+
+    getline(stream, holder);
+    accept_states = utils::split_string(holder, ' ');
+
+    getline(stream, tape);
+
+    vector<vector<int>> transition_matrix_temp(input_alphabet.size(),
+                                               vector<int>(states.size(), 0));
+    while(getline(stream, holder)){
+
+       vector tokens = utils::split_string(holder, ' ');
+
+       //Convert tokens to entries in state transition matrix
+
+    }
+
+}
+
+TuringMachine(string filename){
+    ifstream stream {filename};
+}
+
+~TuringMachine(){
+}
+
+string cur_state{ return states[cur_state]; }
+
+void set_cur_state(string c){
+
+    int index = find(states.begin(), states.end(), c);
+    cur_state_index = index;
+
+}
---
+++ src/turing_machine/turing_machine.hpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "string_utils.hpp"
+
+class TuringMachine{
+    public:
+
+        std::string str();
+
+        std::string input_alphabet;
+        std::string tape;
+
+        char blank_symbol;
+
+        std::vector<std::string> states;
+        std::vector<std::string> accept_states;
+
+        std::vector<std::vector<int>> state_transition_matrix;
+        
+        int head_pos;
+        int cur_state_index;
+
+        TuringMachine(std::vector<std::string> states, std::string input_alphabet,
+                      std::vector<std::vector<int>> state_transition_matrix,
+                      std::string start_state, std::vector<std::string> accept_states,
+                      std::string tape, char blank_symbol,
+                      int head_pos);
+        //Load from file
+        TuringMachine(std::ifstream stream);
+        TuringMachine(std::string filename);
+        ~TuringMachine();
+
+        std::string cur_state();
+        void set_cur_state(std::string s);
+
+        void str();
+};
