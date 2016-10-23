795875368d9468344178c394bd54d25600399958
Genesis Ian M. Palaganas <palaganas.genesis@gmail.com>
2016-10-24 03:01:02 +0800

    Move sample files to src

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    TuringMachine tm = TuringMachine("../samples/sample1.txt");     
+    TuringMachine tm = TuringMachine("samples/sample1.txt");     
 
     cout << tm.str() << endl;
 
